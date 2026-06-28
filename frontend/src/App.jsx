944a7b93fd0399d7c1a48d461c940d1f3881ecc4
andr3s005 <147217934+andr3s005@users.noreply.github.com>
2026-06-27 20:33:04 -0600

    Creamos ProtectedRouted, valdidamos la proteccion de rutas en App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,64 @@
-import { Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { useAuthStore } from "./store/authStore";
+import { getMeRequest } from "./services/authService";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
+  const [checkingSession, setCheckingSession] = useState(true);
+  const setUser = useAuthStore((state) => state.setUser);
+  const logout = useAuthStore((state) => state.logout);
+
+  useEffect(() => {
+    async function checkSession() {
+      const token = localStorage.getItem("access_token");
+
+      if (!token) {
+        setCheckingSession(false);
+        return;
+      }
+
+      try {
+        const data = await getMeRequest();
+        setUser(data.profile);
+      } catch (err) {
+        logout();
+      } finally {
+        setCheckingSession(false);
+      }
+    }
+
+    checkSession();
+  }, []);
+
+  if (checkingSession) {
+    return (
+      <div className="min-h-screen bg-bg flex items-center justify-center">
+        <p className="text-text-secondary font-body">Cargando...</p>
+      </div>
+    );
+  }
+
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<div className="text-white p-8">Página de Registro (próximamente)</div>} />
-      <Route path="/dashboard" element={<div className="text-white p-8">Dashboard (próximamente)</div>} />
-      <Route path="/" element={<div className="text-white p-8">Home (próximamente)</div>} />
+      <Route path="/register" element={<Register />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <div className="text-white p-8">Dashboard (próximamente)</div>
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/"
+        element={<div className="text-white p-8">Home (próximamente)</div>}
+      />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
