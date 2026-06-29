63a626378c1314bc10a5d90b2e4d57fd832b6ea0
andr3s005 <147217934+andr3s005@users.noreply.github.com>
2026-06-28 18:47:50 -0600

    Arreglamos la funcion en themeStore

--- frontend/src/store/themeStore.js
+++ frontend/src/store/themeStore.js
@@ -1,24 +1,24 @@
-import { create } from 'zustand';
+import { create } from 'zustand'
 
 function getInitialTheme() {
-    const saved = localStorage.getItem('theme');
-    return saved || 'dark'
+  const saved = localStorage.getItem('theme')
+  return saved || 'dark'
 }
 
-export const useThemeStore = create ((set) => ({
-    theme: getInitialTheme(),
+export const useThemeStore = create((set) => ({
+  theme: getInitialTheme(),
 
-    toogleTheme: () => {
-        set((state) => {
-            const newTheme = state.theme === 'dark' ? 'light' : 'dark'
-            localStorage.setItem('theme', newTheme)
-            document.documentElement.classList.toogle('light', newTheme === 'light')
-            return { theme: newTheme }
-        })
-    },
+  toggleTheme: () => {
+    set((state) => {
+      const newTheme = state.theme === 'dark' ? 'light' : 'dark'
+      localStorage.setItem('theme', newTheme)
+      document.documentElement.classList.toggle('light', newTheme === 'light')
+      return { theme: newTheme }
+    })
+  },
 
-    applyTheme: () => {
-        const saved = getInitialTheme()
-        document.documentElement.classList.toogle('light', saved === 'light')
-    },
+  applyStoredTheme: () => {
+    const saved = getInitialTheme()
+    document.documentElement.classList.toggle('light', saved === 'light')
+  },
 }))
